4611b8cc42ef88aa1970b1b1dce2b6b83c00f2e9
Pedro Gonzalez <pedro.gch01@gmail.com>
2023-05-31 14:35:37 -0300

    se agrega al modelo de personaje atributos

--- api/database/models/personaje.js
+++ api/database/models/personaje.js
@@ -14,15 +14,32 @@ module.exports = (sequelize, DataTypes) => {
     }
   }
   Personaje.init({
+    jugador_id: DataTypes.INTEGER,
+    cultura_id: DataTypes.INTEGER,
+    especie_id: DataTypes.INTEGER,
+    raza_id: DataTypes.INTEGER,
+    edad: DataTypes.INTEGER,
+    peso: DataTypes.INTEGER,
+    religion_id: DataTypes.INTEGER,
+    profesion_padre_id: DataTypes.INTEGER,
+    profesion_madre_id: DataTypes.INTEGER,
+    profesion_aventurero: DataTypes.INTEGER,
     nombre: DataTypes.STRING,
-    cultura: DataTypes.STRING,
+    cultura_id: DataTypes.STRING,
     fue: DataTypes.SMALLINT,
     con: DataTypes.SMALLINT,
     des: DataTypes.SMALLINT,
     per: DataTypes.SMALLINT,
     asp: DataTypes.SMALLINT,
     tam: DataTypes.SMALLINT,
-    int: DataTypes.SMALLINT
+    int: DataTypes.SMALLINT,
+    o_fue: DataTypes.SMALLINT,
+    o_con: DataTypes.SMALLINT,
+    o_des: DataTypes.SMALLINT,
+    o_per: DataTypes.SMALLINT,
+    o_asp: DataTypes.SMALLINT,
+    o_tam: DataTypes.SMALLINT,
+    o_int: DataTypes.SMALLINT
   }, {
     sequelize,
     modelName: 'Personaje',
