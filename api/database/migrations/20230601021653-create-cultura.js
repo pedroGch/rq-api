4891158c2bedf81dda37d7e3fbce5a6733de850d
Pedro Gonzalez <pedro.gch01@gmail.com>
2023-06-01 13:00:47 -0300

    migracion de cultura

---
+++ api/database/migrations/20230601021653-create-cultura.js
@@ -0,0 +1,41 @@
+'use strict';
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    await queryInterface.createTable('Culturas', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      nombre: {
+        allowNull: false,
+        type: Sequelize.STRING
+      },
+      desde: {
+        allowNull: false,
+        type: Sequelize.SMALLINT
+      },
+      hasta: {
+        allowNull: false,
+        type: Sequelize.SMALLINT
+      },
+      profesion_id: {
+        allowNull: false,
+        type: Sequelize.SMALLINT
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      }
+    });
+  },
+  async down(queryInterface, Sequelize) {
+    await queryInterface.dropTable('Culturas');
+  }
+};
