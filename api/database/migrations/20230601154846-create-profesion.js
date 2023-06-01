852622f794b5a3de7a9a25ff37f529ad86d6cea2
Pedro Gonzalez <pedro.gch01@gmail.com>
2023-06-01 13:00:34 -0300

    migracion de profesion

---
+++ api/database/migrations/20230601154846-create-profesion.js
@@ -0,0 +1,34 @@
+'use strict';
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    await queryInterface.createTable('Profesions', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      nombre: {
+        type: Sequelize.STRING
+      },
+      desde: {
+        type: Sequelize.SMALLINT
+      },
+      hasta: {
+        type: Sequelize.SMALLINT
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      }
+    });
+  },
+  async down(queryInterface, Sequelize) {
+    await queryInterface.dropTable('Profesions');
+  }
+};
