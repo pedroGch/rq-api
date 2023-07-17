f9f37af2ed0f382fcbd19209b83cf9fa16a2fb48
Pedro Gonzalez <pedro.gch01@gmail.com>
2023-07-16 21:10:09 -0300

    correcion uno a uno con personaje

--- api/database/models/cultura.js
+++ api/database/models/cultura.js
@@ -12,7 +12,7 @@ module.exports = (sequelize, DataTypes) => {
     static associate(models) {
       // define association here
       Cultura.hasMany(models.Profesion)
-      Cultura.belongsTo(models.Personaje)
+      Cultura.hasOne(models.Personaje)
     }
   }
   Cultura.init({
