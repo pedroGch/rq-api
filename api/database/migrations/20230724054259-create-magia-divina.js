2d22625c431e788131716056c202d8f51fa5fe97
Pedro Gonzalez <pedro.gch01@gmail.com>
2023-07-24 02:50:59 -0300

    se corrigen los nombres de las tablas

--- api/database/migrations/20230724054259-create-magia-divina.js
+++ api/database/migrations/20230724054259-create-magia-divina.js
@@ -2,7 +2,7 @@
 /** @type {import('sequelize-cli').Migration} */
 module.exports = {
   async up(queryInterface, Sequelize) {
-    await queryInterface.createTable('MagiaDivinas', {
+    await queryInterface.createTable('MagiaDivina', {
       id: {
         allowNull: false,
         autoIncrement: true,
@@ -41,6 +41,6 @@ module.exports = {
     });
   },
   async down(queryInterface, Sequelize) {
-    await queryInterface.dropTable('MagiaDivinas');
+    await queryInterface.dropTable('MagiaDivina');
   }
 };
