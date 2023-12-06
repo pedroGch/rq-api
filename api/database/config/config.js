4d48cd1f4b15997c6e834f7af077a5a309796309
Pedro Gonzalez <pedro.gch01@gmail.com>
2023-12-05 21:33:11 -0300

    agrego uso de db local y uso .env

--- api/database/config/config.js
+++ api/database/config/config.js
@@ -1,12 +1,22 @@
 require('dotenv').config();
 
 module.exports = {
+  // "development":{
+  //   "url": process.env.URL_PROD,
+  //   "dialect": process.env.DIALECT_PROD,
+  //   "dialectOptions":{
+  //     "ssl":{
+  //       "required": true,
+  //       "rejectUnauthorized": false
+  //     }
+  //   }
+  // },
   "development":{
-    "username":process.env.USERMANE,
-    "password":"",
-    "database":process.env.DATABASE,
-    "host": process.env.HOST,
-    "dialect":process.env.DIALECT
+    "username": "root",
+    "password": null,
+    "database": "rq_api",
+    "host": "127.0.0.1",
+    "dialect": "mysql"
   },
   "test": {
     "username": "root",
