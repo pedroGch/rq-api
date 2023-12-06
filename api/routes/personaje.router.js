b80a495b5f8490ff9f6d0eb7fa8f7416eb3850fa
Pedro Gonzalez <pedro.gch01@gmail.com>
2023-12-05 21:31:40 -0300

    arreglo uris, modularizo más

--- api/routes/personaje.router.js
+++ api/routes/personaje.router.js
@@ -1,44 +1,29 @@
 const express = require('express');
 const router = express.Router();
-const PersonajeService = require('./../services/personaje.service')
 const validatorHandler = require('./../middlewares/validator.handler')
 const {createPersonaje} = require('./../schemas/personaje.schema')
-const {create,  getPersonajeId,  getPersonajes,  update,  getAllStats} = require('./../controller/personaje.controller')
+const { getPersonajeId,  getPersonajes,  update,  getAllStats, createdCharacter} = require('./../controller/personaje.controller')
 
 
 
 /**
  * trae todos los personajes
  */
-router.get('/', async (req, res) => {
-  //const personaje = await pService.getPersonajes()
-  getPersonajes
-  res.json(getPersonajes)
-})
+router.get('/', getPersonajes)
 
 /**
  * trae a un personaje por su id
  */
-router.get('/:id', async (req, res, next) => {
-  try {
-    const { id } = req.params;
-    const p = await getPersonajeId(id)
-    res.json(p);
-  } catch (error) {
-    next(error)
-  }
+router.get('/:id', getPersonajeId)
+
+/**
+ * Crea un personaje
+ */
+router.post('/', createdCharacter)
 
 
-})
 
-router.post('/', 
-//validatorHandler(createPersonaje, 'body'),
-async (req, res) =>{
-  const body = req.body;
-  let personaje = await create(body);
-  personaje ? res.status(201).json(personaje) : res.status(500).json("los dioses no aceptan a tu personaje")
-  
-})
+
 
 /**
  * trea la info de un arma especifica de un personaje
