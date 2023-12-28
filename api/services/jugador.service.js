b8f36730ffeb76c987d1af7cc8074a3673ce80d4
Pedro Gonzalez <pedro.gch01@gmail.com>
2023-12-27 23:29:36 -0300

    jugador service, login

--- api/services/jugador.service.js
+++ api/services/jugador.service.js
@@ -1,5 +1,5 @@
+const jwt = require('jsonwebtoken')
 const bcrypt = require('bcrypt')
-const { func } = require('joi')
 const model = require('../database/models/index')
 
 async function  altaJugador (data) {
@@ -9,6 +9,28 @@ async function  altaJugador (data) {
   return jugador
 }
 
+async function loginJugador(data) {
+  const jugador = await obtenerJugador(data.nombre)
+  if (!jugador){
+    throw {mensaje : 'usuario o contraseña incorrecta'}
+  }
+  
+  if (!bcrypt.compareSync(data.password, jugador.get().password)){
+    throw {mensaje : 'usuario o contraseña incorrecta'}  
+  }
+
+  const token = jwt.sign(data, "glorantha", {expiresIn:86400})
+  
+  data = {...data, password:undefined, token: token}
+  
+  return data
+}
+
+async function obtenerJugador(nombre) {
+  const jugador = await model.Jugador.findOne({where: {nombre: nombre}})
+  return jugador
+}
 module.exports = {
-  altaJugador
+  altaJugador,
+  loginJugador
 }
